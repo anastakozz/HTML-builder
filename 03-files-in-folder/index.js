daca6680e52f7540b65fa5b9fcf03ac4978f4a41
anastakozz <shvedagon@gmail.com>
2023-05-06 17:06:22 +0300

    feat: 03-files-in-folder done

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+// const fs = require('fs');
+const fsPromise = require('fs/promises');
+const path = require('path');
+
+fsPromise.readdir(path.join(__dirname, './secret-folder'),
+  { withFileTypes: true }).then(files => {
+  files.forEach( file => {
+    if(file.isFile()){
+      let ext = path.extname(file.name);
+      let name = path.basename(file.name, ext);
+      fsPromise.stat(path.join(__dirname, 'secret-folder', file.name)).then(data => {
+        console.log(name + ' - ' + ext.replace('.', '') + ' - ' + data.size);
+      });
+    }
+  });
+}
+);
