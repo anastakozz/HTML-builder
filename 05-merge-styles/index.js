284eb947647de1c77bc0b38d9bc69867c14424b1
anastakozz <shvedagon@gmail.com>
2023-05-07 18:27:58 +0300

    feat: 05-merge-styles done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
+const output = fs.createWriteStream(path.join(__dirname,'project-dist', 'bundle.css'));
+
+fsPromises.readdir(path.join(__dirname, 'styles'),
+  { withFileTypes: true }).then( files => {
+  files.forEach( file => {
+    let ext = path.extname(file.name);
+    if(file.isFile() && ext === '.css'){
+      console.log(file.name);
+      fsPromises.readFile(path.join(__dirname, 'styles', file.name), 
+        { encoding: 'utf8' }).then( data => {
+        output.write(data);
+      });
+    }
+            
+  }
+  );
+});
