a59ce8b77b40b228c94cde27d2e9605bc4e73b2b
anastakozz <shvedagon@gmail.com>
2023-05-08 18:24:28 +0300

    fix: clear folder in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,12 +1,16 @@
 const fsPromises = require('fs/promises');
 const path = require('path');
 
-fsPromises.mkdir(path.join(__dirname, 'files-copy'),
-{ recursive: true }).then (() => {
+fsPromises.rm(path.join(__dirname, 'files-copy'),
+  { recursive: true, force: true }).then(() => {
+  fsPromises.mkdir(path.join(__dirname, 'files-copy'),
+    { recursive: true }).then (() => {
     fsPromises.readdir(path.join(__dirname, 'files'),
-    { withFileTypes: true }).then(files => {
-        files.forEach(file => {
-            fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name))
-        })
-    })
+      { withFileTypes: true }).then(files => {
+      files.forEach(file => {
+        fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name));
+      });
+    });
+  });
 });
+
